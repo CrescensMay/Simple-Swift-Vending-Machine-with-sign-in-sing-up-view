d6c3f1ea48d793cc33095ca0b35bcbd7bfe1971f
Fidele <crescens.kob@outlook.fr>
2017-10-05 05:43:15 +0800

    handling errors, casting and return the dictionary

--- VendingMachine/VendingMachine.swift
+++ VendingMachine/VendingMachine.swift
@@ -59,9 +59,11 @@ class PlistConverter {
             throw IventoryError.invalidResource
         }
         
-        guard let dictionary = NSDictionary(contentsOfFile: path)else {
+        guard let dictionary = NSDictionary(contentsOfFile: path) as? [String: AnyObject] else {
             throw IventoryError.conversionFailure
         }
+        
+        return dictionary
     }
 }
 
