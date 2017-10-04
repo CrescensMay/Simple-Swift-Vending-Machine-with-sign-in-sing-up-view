281e34c515656b3aff705df2f803166df3c7f7af
Fidele <crescens.kob@outlook.fr>
2017-10-05 04:11:15 +0800

    initial commit

---
+++ VendingMachine/VendingItemCell.swift
@@ -0,0 +1,14 @@
+//
+//  VendingItemCell.swift
+//  VendingMachine
+//
+//  Created by Mac on 5/10/17.
+//  Copyright © 2017 SmartSoft All rights reserved.
+//
+
+import UIKit
+
+class VendingItemCell: UICollectionViewCell {
+    
+    @IBOutlet weak var iconView: UIImageView!
+}
---
+++ VendingMachine/VendingMachine.swift
@@ -0,0 +1,9 @@
+//
+//  VendingMachine.swift
+//  VendingMachine
+//
+//  Created by Mac on 05/10/2017.
+//  Copyright © 2017 SmartSoft. All rights reserved.
+//
+
+
