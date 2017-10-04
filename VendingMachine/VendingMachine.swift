66a1c082a5a1dbdf12672aa2917962ba218560a0
Fidele <crescens.kob@outlook.fr>
2017-10-05 04:21:50 +0800

    created an enum to hold all the items, vending machine with vend and deposit features

--- VendingMachine/VendingMachine.swift
+++ VendingMachine/VendingMachine.swift
@@ -6,4 +6,35 @@
 //  Copyright © 2017 SmartSoft. All rights reserved.
 //
 
+import Foundation
 
+enum VendingSelection {
+    case soda
+    case dietSoda
+    case chips
+    case cookie
+    case sandwich
+    case wrap
+    case candyBar
+    case popTart
+    case water
+    case fruitJuice
+    case sportsDrink
+    case gum
+}
+
+protocol VendingItem {
+    var price: Double { get }
+    var quantity: Int { get set }
+}
+
+protocol VendingMachine {
+    var selection: [VendingSelection] { get }
+    var inventory: [VendingSelection: VendingItem] { get set }
+    var amountDeposited: Double { get set }
+    
+    init(inventory: [VendingSelection: VendingItem])
+    func vend(_ quantity: Int, _ selection: VendingSelection) throws
+    func deposit(_ amount: Double)
+    
+}
