9e5bc8ca2da068c054aeacb05785890dadbba09f
Fidele <crescens.kob@outlook.fr>
2017-10-06 03:15:18 +0800

    Created a new view controller and wired up the dismiss button, and also add funds button, to add up the deposit

---
+++ VendingMachine/DepositController.swift
@@ -0,0 +1,38 @@
+//
+//  DepositController.swift
+//  VendingMachine
+//
+//  Created by Mac on 06/10/2017.
+//  Copyright © 2017 SmartSoft. All rights reserved.
+//
+
+import UIKit
+
+class DepositController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func dismiss(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
